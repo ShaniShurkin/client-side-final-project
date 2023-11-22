c5fad579c970614427c963333b3596875fd7fbb4
ShaniShurkin <shanishurkin@gmail.com>
2023-11-22 11:38:31 +0200

    Added removing to categories

--- src/components/registration/MenuSettings.js
+++ src/components/registration/MenuSettings.js
@@ -4,7 +4,7 @@ import { useSelector } from "react-redux";
 import { urlMenu } from "../../endpoints";
 import dictionary from "../dictionary";
 import { useForm } from 'react-hook-form';
-import { Form, Button, ListGroup, Alert } from 'react-bootstrap';
+import { Form, Button, ListGroup, Alert, Modal } from 'react-bootstrap';
 import { createRoutesFromChildren, useNavigate } from 'react-router-dom';
 import React from "react";
 
@@ -24,7 +24,7 @@ const MenuSettings = React.memo((props) => {
     const [categories, setCategories] = useState({});
     useEffect(() => {
         setCategories({
-            1: "Shani", 2: "Tamar", 3: "Tamar and Shani😁"
+            1: "Shani", 2: "Tamar", 3: "Tamar and Shani😁", 4: "Shachar", 5: "Shurkin"
         })
         if (!verifyDailyCalorieCount()) {
             setError('meals', {
@@ -35,7 +35,6 @@ const MenuSettings = React.memo((props) => {
     }, [])
 
     const title = "title";
-    const [show, setShow] = useState(true);
     const [editing, setEditing] = useState(false);
     const [newTitle, setNewTitle] = useState(title);
 
@@ -81,6 +80,24 @@ const MenuSettings = React.memo((props) => {
         let value = e.target.value
         setMeals(meals, meals[name][lang] = value)
     }
+    function handleRemove(name, category) {
+        if (meals[name].categories.length == 1) {
+            //TODO: handle this error
+            console.log("You can't remove last item")
+            return
+        }
+        const updatedMeal = { ...meals[name], categories: meals[name].categories.filter(cat => cat !== category) };
+        let copiedMeals = { ...meals }
+        copiedMeals[name] = updatedMeal
+        setMeals({ ...copiedMeals });
+        console.log(meals)
+    }
+    const [show, setShow] = useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+    function handleAdd(name) {
+    }
 
     // const f = (code) => {
     //     fetch(urlMenu + `meals/${code}`, {
@@ -119,10 +136,40 @@ const MenuSettings = React.memo((props) => {
                             <Form.Control type="number" min={0} max={100} defaultValue={meals[name]["calories"] * 100} onChange={(e) => changeCalories(e, name)} />
                         </Form.Group>
                         <div className="categories">
-                            <Form.Label><b>{`${dictionary.categories[lang]}:`}</b></Form.Label>
+                            <Form.Label>
+                                <span><b>{`${dictionary.categories[lang]}:`}</b></span>
+                                <Button onClick={handleShow}>+</Button>
+                                <Modal show={show} onHide={handleClose}>
+                                    <Modal.Header closeButton>
+                                        <Modal.Title>Modal heading</Modal.Title>
+                                    </Modal.Header>
+                                    <Modal.Body>
+                                        {
+                                            // categories.map((key, value)=>{
+                                            //     <Form.Check // prettier-ignore
+                                            //     type='checkbox'
+                                            //     label={value}
+                                            // />
+                                            // })
+                                        }
+                                       
+                                    </Modal.Body>
+                                    <Modal.Footer>
+                                        <Button variant="secondary" onClick={handleClose}>
+                                            Close
+                                        </Button>
+                                        <Button variant="primary" onClick={handleClose}>
+                                            Save Changes
+                                        </Button>
+                                    </Modal.Footer>
+                                </Modal>
+                            </Form.Label>
                             {meals[name]["categories"].map((cat) => (
                                 <ListGroup controlId={`${meals[name]} categories ${cat}`}>
-                                    <ListGroup.Item>{categories[cat]}</ListGroup.Item>
+                                    <ListGroup.Item>
+                                        <span>{categories[cat]}</span>
+                                        <Button type="button" onClick={() => handleRemove(name, cat)}>-</Button>
+                                    </ListGroup.Item>
                                 </ListGroup>
                             ))}
                         </div>
@@ -136,6 +183,7 @@ const MenuSettings = React.memo((props) => {
                     {dictionary.next[lang]}
                 </Button>
             </Form>
+
         </>
 
     );
